528a6d10a10264c3062ea65299ea20fe981b90d5
kjirawat <158753919+kjirawat@users.noreply.github.com>
2024-09-28 03:51:50 +0000

    lab ai chatbot

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🎈 My chatbot app")
+# st.write(
+#    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# )
+
+#if user_input := st.text_input("You: ", placeholder="Type your message here..."):
+#    st.write(f"User Input: {user_input}")
+
+title = st.text_input("Movie title", "Life of Brian")
+st.write("The current movie title is", title)
+
+#st.text_input()
+#s1 = st.text_input()
+#st.write(s1)
